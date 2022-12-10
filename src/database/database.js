212af217509794c16b10120c805c48be219d6b0e
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-10 20:58:25 -0300

    connectionDB

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,10 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const { Pool } = pkg;
 
-	const connection = new Pool({
+	export const connectionDB = new Pool({
         connectionString: process.env.DATABASE_URL,
       });
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import connection from "./database/database.js";
+import { connectionDB } from "./database/database.js";
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
+app.use(express.json());
