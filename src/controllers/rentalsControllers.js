8969f542e740960ff65cbf6a0d2d02fc18adf399
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-13 17:40:10 -0300

    Return rentals route

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,4 +1,5 @@
 import { connectionDB } from "../database/database.js";
+import dayjs from "dayjs";
 
 export async function getRentals(req, res){
     const findCustomer = req.query.customerId;
@@ -52,6 +53,38 @@ export async function postRentals(req, res){
     try{
         await connectionDB.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7);`,[customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee]);
         return res.sendStatus(201)
+    }catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+
+}
+
+export async function returnRental(req, res){
+    const id = req.params.id;
+    try{
+        const rental = await connectionDB.query(`SELECT * FROM rentals WHERE id=$1;`,[id]);
+        if(!rental){
+            return res.sendStatus(404);
+        }
+        if(rental.rows[0].returnDate){
+            return res.sendStatus(400);
+        }
+        const {customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee} = rental.rows[0];
+        const newReturnDate = dayjs().format(`YYYY/MM/DD`);
+
+        const timeRented = dayjs().diff(rentDate, 'day');
+       let newDelayFee = null;
+        if(timeRented>daysRented){
+            
+           const dailyPrice = Math.trunc(originalPrice/daysRented);
+           const pastDate = timeRented - daysRented;
+           newDelayFee = dailyPrice*pastDate;
+        }
+
+        await connectionDB.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3;`,[newReturnDate, newDelayFee, id]);
+        return res.sendStatus(201)
+
     }catch(err){
         console.log(err);
         return res.sendStatus(500);
--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getRentals, postRentals } from "../controllers/rentalsControllers.js";
+import { getRentals, postRentals, returnRental } from "../controllers/rentalsControllers.js";
 import { rentalSchemaValidation } from "../middlewares/rentalsMiddlewares.js";
 
 const router = Router();
 
 router.get("/rentals", getRentals);
 router.post("/rentals", rentalSchemaValidation, postRentals);
+router.post("/rentals/:id/return", returnRental);
 
 export default router;
